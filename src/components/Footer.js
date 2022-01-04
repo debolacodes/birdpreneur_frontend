6fe69d887c9b9021f35f73065d1a9f18a8f5703d
debolacodes <42511408+debolacodes@users.noreply.github.com>
2022-01-04 07:16:56 +0100

    Added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+<div className="page-footer font-small blue pt-4">
+
+  <div className="container-fluid text-center text-md-left">
+    <div className="row">
+      <div className="col-md-12 mt-md-0 mt-3">
+        <div className='logo_white'></div>
+        <h5 className="text-uppercase">BIRDPRENEUR</h5>
+        <p>Here you can use rows and columns to organize your footer content.</p>
+
+      </div>
+      <hr className="clearfix w-100 d-md-none pb-3" />
+
+
+    </div>
+
+  </div>
+  <div className="footer-copyright text-center py-3"><small>© 2021 Copyright: Birdpreneur Ltd</small>
+  </div>
+
+</div>
+        </div>
+    )
+}
